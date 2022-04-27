c045960287298e59d2dc52844cb722702a1e1826
mgmckinn <mgmckinn2325@me.com>
2022-04-26 21:29:40 -0400

    update package json and server mongoose setting

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+const PORT = process.env.PORT || 27017;
 
 const app = express();
 
@@ -16,14 +15,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology:true,
-  useFindAndModify: false
-});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/budget",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+   
+  }
+);
 
 // routes
-app.use(require("./routes/api"));
+app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
