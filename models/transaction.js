321c2d48fd09f3e653927aa869e5008fd2ca865e
Mitch <mgmckinn2325@me.com>
2022-04-30 12:16:20 -0400

    fixed mongo db windows pc

--- models/transaction.js
+++ models/transaction.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 
+const Schema = mongoose.Schema;
 
 
-const transactionSchema = new mongoose.Schema(
+
+const transactionSchema = new Schema(
   {
     name: {
       type: String,
--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 27017;
+const PORT = process.env.PORT || 3001;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budgetDB";
 
 const app = express();
 
@@ -15,13 +16,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/budget",
+mongoose.connect(MONGODB_URI,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true
-   
-  }
-);
+});
 
 // routes
 app.use(require("./routes/api.js"));
